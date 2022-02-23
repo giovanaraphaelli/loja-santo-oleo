508e19d7edc846d08767f734d33a95a3e49eddf6
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-02-23 17:23:32 -0300

    ajustes altera-valor

--- js/altera-valor.js
+++ js/altera-valor.js
@@ -7,22 +7,26 @@ v1.addEventListener("click", function () {
   document.getElementById("preco1").innerHTML = "R$ 17,88";
   document.getElementById("preco2").innerHTML = "R$ 30,00";
   document.getElementById("preco3").innerHTML = "R$ 18,32";
+  document.getElementById("preco4").innerHTML = "3x RS 3,27";
 });
 
 v2.addEventListener("click", function () {
   document.getElementById("preco1").innerHTML = "R$ 32,88";
   document.getElementById("preco2").innerHTML = "R$64,00";
   document.getElementById("preco3").innerHTML = "R$ 35,32";
+  document.getElementById("preco4").innerHTML = "3x RS 11,78";
 });
 
 v3.addEventListener("click", function () {
   document.getElementById("preco1").innerHTML = "R$ 54,88";
   document.getElementById("preco2").innerHTML = "R$ 71,00";
   document.getElementById("preco3").innerHTML = "R$ 58,45";
+  document.getElementById("preco4").innerHTML = "3x RS 19,48";
 });
 
 v4.addEventListener("click", function () {
   document.getElementById("preco1").innerHTML = "R$ 83,88";
   document.getElementById("preco2").innerHTML = "R$ 102,00";
   document.getElementById("preco3").innerHTML = "R$ 91,80";
+  document.getElementById("preco4").innerHTML = "3x RS 30,06";
 });
