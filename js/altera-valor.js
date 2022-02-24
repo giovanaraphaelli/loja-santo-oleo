0e16da37a59d7c35aef9401d5ab02b237d500a3f
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-02-24 18:19:27 -0300

    ajuste altera-valor

--- js/altera-valor.js
+++ js/altera-valor.js
@@ -3,25 +3,51 @@ const v2 = document.querySelector("#n2");
 const v3 = document.querySelector("#n3");
 const v4 = document.querySelector("#n4");
 
-let v1p1 = 17.88;
-let v1p2 = 30.05;
-let v1p3 = 18.32;
-let v1p4 = 3.27;
+let opcaoMl = 0;
 
-let v2p1 = 32.62;
-let v2p2 = 64.08;
-let v2p3 = 35.32;
-let v2p4 = 11.78;
-
-let v3p1 = 54.49;
-let v3p2 = 71.09;
-let v3p3 = 58.45;
-let v3p4 = 19.48;
-
-let v4p1 = 83.23;
-let v4p2 = 102.65;
-let v4p3 = 91.87;
-let v4p4 = 30.06;
+function calculaMl(precoRequisitado) {
+  if (precoRequisitado == "preco1") {
+    if (opcaoMl == 0) {
+      return 17.88;
+    } else if (opcaoMl == 1) {
+      return 32.62;
+    } else if (opcaoMl == 2) {
+      return 54.49;
+    } else if (opcaoMl == 3) {
+      return 83.23;
+    }
+  } else if (precoRequisitado == "preco2") {
+    if (opcaoMl == 0) {
+      return 30.05;
+    } else if (opcaoMl == 1) {
+      return 64.08;
+    } else if (opcaoMl == 2) {
+      return 71.09;
+    } else if (opcaoMl == 3) {
+      return 102.65;
+    }
+  } else if (precoRequisitado == "preco3") {
+    if (opcaoMl == 0) {
+      return 18.32;
+    } else if (opcaoMl == 1) {
+      return 35.32;
+    } else if (opcaoMl == 2) {
+      return 58.45;
+    } else if (opcaoMl == 3) {
+      return 91.87;
+    }
+  } else if (precoRequisitado == "preco4") {
+    if (opcaoMl == 0) {
+      return 3.27;
+    } else if (opcaoMl == 1) {
+      return 11.78;
+    } else if (opcaoMl == 2) {
+      return 19.48;
+    } else if (opcaoMl == 3) {
+      return 30.06;
+    }
+  }
+}
 
 let p1atual = document.getElementById("preco1");
 let p2atual = document.getElementById("preco2");
@@ -29,47 +55,46 @@ let p3atual = document.getElementById("preco3");
 let p4atual = document.getElementById("preco4");
 
 v1.addEventListener("click", function () {
-  p1atual.innerHTML = `R$ ${v1p1}`;
-  p2atual.innerHTML = `R$ ${v1p2}`;
-  p3atual.innerHTML = `R$ ${v1p3}`;
-  p4atual.innerHTML = `3x R$ ${v1p4}`;
+  opcaoMl = 0;
+  atualizaPreco();
 });
 
 v2.addEventListener("click", function () {
-  p1atual.innerHTML = `R$ ${v2p1}`;
-  p2atual.innerHTML = `R$ ${v2p2}`;
-  p3atual.innerHTML = `R$ ${v2p3}`;
-  p4atual.innerHTML = `3x R$ ${v2p4}`;
+  opcaoMl = 1;
+  atualizaPreco();
 });
 
 v3.addEventListener("click", function () {
-  p1atual.innerHTML = `R$ ${v3p1}`;
-  p2atual.innerHTML = `R$ ${v3p2}`;
-  p3atual.innerHTML = `R$ ${v3p3}`;
-  p4atual.innerHTML = `3x R$ ${v3p4}`;
+  opcaoMl = 2;
+  atualizaPreco();
 });
 
 v4.addEventListener("click", function () {
-  p1atual.innerHTML = `R$ ${v4p1}`;
-  p2atual.innerHTML = `R$ ${v4p2}`;
-  p3atual.innerHTML = `R$ ${v4p3}`;
-  p4atual.innerHTML = `3x R$ ${v4p4}`;
+  opcaoMl = 3;
+  atualizaPreco();
 });
 
 const btnSoma = document.querySelector("#mais");
 const btnMenos = document.querySelector("#menos");
-let mudaNum = 1;
+let qtd = 1;
 
 btnSoma.addEventListener("click", function () {
-  let novoMudaNum = (document.getElementById("num").innerHTML = mudaNum += 1);
-  p1atual.innerHTML = p1atual *= novoMudaNum;
-  p2atual.innerHTML = p2atual *= novoMudaNum;
-  p3atual.innerHTML = p3atual *= novoMudaNum;
-  p4atual.innerHTML = p4atual *= novoMudaNum;
+  qtd += 1;
+  document.getElementById("num").innerHTML = qtd;
+
+  atualizaPreco();
+});
+let teste;
+btnMenos.addEventListener("click", function () {
+  if (qtd > 1) qtd -= 1;
+
+  document.getElementById("num").innerHTML = qtd;
+  atualizaPreco();
 });
 
-btnMenos.addEventListener("click", function () {
-  if (mudaNum > 1) {
-    document.getElementById("num").innerHTML = mudaNum -= 1;
-  }
-});
+function atualizaPreco() {
+  p1atual.innerHTML = `R$ ${(qtd * calculaMl("preco1")).toFixed(2)}`;
+  p2atual.innerHTML = `R$ ${(qtd * calculaMl("preco2")).toFixed(2)}`;
+  p3atual.innerHTML = `R$ ${(qtd * calculaMl("preco3")).toFixed(2)}`;
+  p4atual.innerHTML = `${(qtd * calculaMl("preco4")).toFixed(2)}`;
+}
